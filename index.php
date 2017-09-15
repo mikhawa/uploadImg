9df8b973fb68680ad5bee63f3acdea1caf943737
michaeljpitz <michaeljpitz@gmail.com>
2017-09-15 10:19:20 +0200

    Création controller et déconnexion

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 
-/* 
+/*
  * Contrôleur frontal
  */
 
+// pour session
+session_start();
+
 // on essaye de se connecter
-try{
+try {
     $connect = new PDO('mysql:host=localhost;dbname=mvc_5', "root", "");
 // on récupère l'erreur au cas où    
-}catch(PDOException $e){
+} catch (PDOException $e) {
     // on affiche un message d'erreur
-    echo "<h1>".$e->getMessage()."</h1>";
-    
+    echo "<h1>" . $e->getMessage() . "</h1>";
 }
 // affichage d'erreur sql, pour debuggage (dev)
 $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -21,10 +23,15 @@ require_once 'vendor/autoload.php';
 
 // création du loader twig
 $loader = new Twig_Loader_Filesystem('v');
-$twig = new Twig_Environment($loader/*, array(
-    'cache' => 'cache',
-)*/);
+$twig = new Twig_Environment($loader/* , array(
+          'cache' => 'cache',
+          ) */);
 
-// récupération du contrôleur
-require_once 'c/ImagesController.php';
+// si on essaye de se connecter ou si on est connecté
+if (isset($_POST['login']) || isset($_SESSION['clefUnique'])) {
+    require_once 'c/ConnectController.php';
+} else {
+// récupération du contrôleur anonyme
+    require_once 'c/ImagesController.php';
+}
 
